aaf19a1263459b657921adcd68361673e80597e6
matt overstreet <street.matt.over@gmail.com>
2022-07-31 17:03:57 -0500

    Segment3 added

---
+++ Segment3/sql/epl.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Stats (
+	ID int NOT NULL,
+	Div CHAR(2),
+	Date DATE,
+	HomeTeam CHAR(16),
+	AwayTeam CHAR(16),
+	FTHG INT,
+	FTAG INT,
+	FTR CHAR(2),
+	HTHG INT,
+	HTAG INT,
+	HTR CHAR(2),
+	Referee CHAR(13),
+	HS INT,
+	ASs INT,
+	HST INT,
+	AST INT,
+	HF INT,
+	AF INT,
+	HC INT,
+	AC INT,
+	HY INT,
+	AY INT,
+	HR INT,
+	AR INT,
+	PRIMARY KEY (ID)
+);
+
+CREATE Table Betting (
+	ID int NOT NULL,
+	DIV CHAR(10),
+	B365H DECIMAL,
+	B365D DECIMAL,
+	B365A DECIMAL,
+	VCH DECIMAL,
+	VCD DECIMAL,
+	VCA DECIMAL,
+	PRIMARY KEY (ID)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
